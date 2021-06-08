d57a89930b8ba630edd91fca6901f2df94500f22
kenithigot <65687563+kenithigot@users.noreply.github.com>
2021-06-08 13:31:40 +0800

    Update Key_Checker.java

--- src/logger_system/Key_Checker.java
+++ src/logger_system/Key_Checker.java
@@ -61,11 +61,11 @@ public class Key_Checker {
 		panel_KEY_CHECKER.setBorder(new EtchedBorder(EtchedBorder.RAISED, Color.BLUE, Color.BLUE));
 		frame.getContentPane().add(panel_KEY_CHECKER);
 		
-		JLabel lblMR_KEY_CHECKER = new JLabel(Messages.getString("Key_Checker.0")); //$NON-NLS-1$
+		JLabel lblMR_KEY_CHECKER = new JLabel(Messages.getString("Key_Checker.0")); 
 		panel_KEY_CHECKER.add(lblMR_KEY_CHECKER);
 		lblMR_KEY_CHECKER.setVerticalAlignment(SwingConstants.TOP);
 		lblMR_KEY_CHECKER.setBackground(Color.RED);
-		lblMR_KEY_CHECKER.setFont(new Font( Font.BOLD, 20)); //$NON-NLS-1$
+		lblMR_KEY_CHECKER.setFont(new Font( Font.BOLD, 20)); 
 		
 		JPanel panel_NAME = new JPanel();
 		panel_NAME.setBackground(SystemColor.inactiveCaptionBorder);
@@ -73,9 +73,9 @@ public class Key_Checker {
 		panel_NAME.setBorder(new EtchedBorder(EtchedBorder.RAISED, Color.BLUE, Color.BLUE));
 		frame.getContentPane().add(panel_NAME);
 		
-		JLabel lblNAME = new JLabel(Messages.getString("Key_Checker.2")); //$NON-NLS-1$
+		JLabel lblNAME = new JLabel(Messages.getString("Key_Checker.2")); 
 		panel_NAME.add(lblNAME);
-		lblNAME.setFont(new Font(Messages.getString("Key_Checker.3"), Font.BOLD, 16)); //$NON-NLS-1$
+		lblNAME.setFont(new Font(Messages.getString("Key_Checker.3"), Font.BOLD, 16)); 
 		
 		JPanel panel_KEY_ID = new JPanel();
 		panel_KEY_ID.setBackground(SystemColor.inactiveCaptionBorder);
@@ -83,10 +83,10 @@ public class Key_Checker {
 		panel_KEY_ID.setBorder(new EtchedBorder(EtchedBorder.RAISED, Color.BLUE, Color.BLUE));
 		frame.getContentPane().add(panel_KEY_ID);
 		
-		JLabel lblKEY_ID = new JLabel(Messages.getString("Key_Checker.4")); //$NON-NLS-1$
+		JLabel lblKEY_ID = new JLabel(Messages.getString("Key_Checker.4")); 
 		panel_KEY_ID.add(lblKEY_ID);
 		lblKEY_ID.setVerticalAlignment(SwingConstants.TOP);
-		lblKEY_ID.setFont(new Font(Messages.getString("Key_Checker.5"), Font.BOLD, 16)); //$NON-NLS-1$
+		lblKEY_ID.setFont(new Font(Messages.getString("Key_Checker.5"), Font.BOLD, 16)); 
 		
 		JPanel panel_DATE_RETURNED = new JPanel();
 		panel_DATE_RETURNED.setBackground(SystemColor.inactiveCaptionBorder);
@@ -94,9 +94,9 @@ public class Key_Checker {
 		panel_DATE_RETURNED.setBorder(new EtchedBorder(EtchedBorder.RAISED, Color.BLUE, Color.BLUE));
 		frame.getContentPane().add(panel_DATE_RETURNED);
 		
-		JLabel lblNewLabel = new JLabel(Messages.getString("Key_Checker.6")); //$NON-NLS-1$
+		JLabel lblNewLabel = new JLabel(Messages.getString("Key_Checker.6")); 
 		panel_DATE_RETURNED.add(lblNewLabel);
-		lblNewLabel.setFont(new Font(Messages.getString("Key_Checker.7"), Font.BOLD, 16)); //$NON-NLS-1$
+		lblNewLabel.setFont(new Font(Messages.getString("Key_Checker.7"), Font.BOLD, 16)); 
 		
 		JPanel panel_DATE_RETURN = new JPanel();
 		panel_DATE_RETURN.setBackground(SystemColor.inactiveCaptionBorder);
@@ -104,25 +104,25 @@ public class Key_Checker {
 		panel_DATE_RETURN.setBorder(new EtchedBorder(EtchedBorder.RAISED, Color.BLUE, Color.BLUE));
 		frame.getContentPane().add(panel_DATE_RETURN);
 		
-		JLabel lblDATE_RETURNED = new JLabel(Messages.getString("Key_Checker.8")); //$NON-NLS-1$
+		JLabel lblDATE_RETURNED = new JLabel(Messages.getString("Key_Checker.8")); 
 		panel_DATE_RETURN.add(lblDATE_RETURNED);
-		lblDATE_RETURNED.setFont(new Font(Messages.getString("Key_Checker.9"), Font.BOLD, 16)); //$NON-NLS-1$
+		lblDATE_RETURNED.setFont(new Font(Messages.getString("Key_Checker.9"), Font.BOLD, 16)); 
 		
 		//Logout Button
-		JButton btnLOGOUT = new JButton(Messages.getString("Key_Checker.10")); //$NON-NLS-1$
+		JButton btnLOGOUT = new JButton(Messages.getString("Key_Checker.10")); 
 		btnLOGOUT.setBorder(new BevelBorder(BevelBorder.RAISED, new Color(255, 0, 0), null, null, Color.RED));
 		btnLOGOUT.setBackground(SystemColor.inactiveCaption);
 		btnLOGOUT.setBounds(695, 49, 100, 35);
 		btnLOGOUT.addActionListener(new ActionListener()
 		{private JFrame frame;
 			public void actionPerformed(ActionEvent e) {
-				frame = new JFrame(Messages.getString("Key_Checker.11")); //$NON-NLS-1$
-				if(JOptionPane.showConfirmDialog(frame,  Messages.getString("Key_Checker.12"), Messages.getString("Key_Checker.13"),JOptionPane.YES_NO_OPTION)==JOptionPane.YES_OPTION) { //$NON-NLS-1$ //$NON-NLS-2$
+				frame = new JFrame(Messages.getString("Key_Checker.11")); 
+				if(JOptionPane.showConfirmDialog(frame,  Messages.getString("Key_Checker.12"), Messages.getString("Key_Checker.13"),JOptionPane.YES_NO_OPTION)==JOptionPane.YES_OPTION) { 
 					System.exit(0);
 				}
 			}
 		});
-		btnLOGOUT.setFont(new Font(Messages.getString("Key_Checker.14"), Font.BOLD, 13)); //$NON-NLS-1$
+		btnLOGOUT.setFont(new Font(Messages.getString("Key_Checker.14"), Font.BOLD, 13)); 
 		frame.getContentPane().add(btnLOGOUT);
 		
 		txtNAME_SLOT = new JTextField();
@@ -150,47 +150,47 @@ public class Key_Checker {
 		txtDATE_RETURNED_SLOT.setColumns(10);
 		  
 		//Button ADD INFORMATION
-		JButton btnADD = new JButton(Messages.getString("Key_Checker.15")); //$NON-NLS-1$
+		JButton btnADD = new JButton(Messages.getString("Key_Checker.15")); 
 		btnADD.setBackground(SystemColor.inactiveCaption);
 		btnADD.setBorder(new BevelBorder(BevelBorder.RAISED, Color.BLUE, null, null, Color.BLUE));
 		btnADD.setBounds(649, 164, 66, 22);
 		frame.getContentPane().add(btnADD);
 		btnADD.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {	
-				if (txtNAME_SLOT.getText().equals(Messages.getString("Key_Checker.16")) || txtKEY_ID_SLOT.getText().equals(Messages.getString("Key_Checker.17")) || txtDATE_BARROWED_SLOT.getText().equals(Messages.getString("Key_Checker.18")) || txtDATE_RETURNED_SLOT.getText().equals(Messages.getString("Key_Checker.19"))) //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+				if (txtNAME_SLOT.getText().equals(Messages.getString("Key_Checker.16")) || txtKEY_ID_SLOT.getText().equals(Messages.getString("Key_Checker.17")) || txtDATE_BARROWED_SLOT.getText().equals(Messages.getString("Key_Checker.18")) || txtDATE_RETURNED_SLOT.getText().equals(Messages.getString("Key_Checker.19"))) 
 				 {
-					JOptionPane.showMessageDialog(null, Messages.getString("Key_Checker.20"));	 //$NON-NLS-1$
+					JOptionPane.showMessageDialog(null, Messages.getString("Key_Checker.20"));	
 					}
 				else {
 				DefaultTableModel model = (DefaultTableModel)table.getModel();
 				model.addRow(new Object [] { txtNAME_SLOT.getText(),txtKEY_ID_SLOT.getText(), txtDATE_BARROWED_SLOT.getText(), txtDATE_RETURNED_SLOT.getText()});
-					JOptionPane.showMessageDialog(null, Messages.getString("Key_Checker.21"));	 //$NON-NLS-1$
+					JOptionPane.showMessageDialog(null, Messages.getString("Key_Checker.21"));	 
 				}
 			}	
 		});
 		btnADD.setForeground(Color.BLACK);
-		btnADD.setFont(new Font(Messages.getString("Key_Checker.22"), Font.BOLD, 7)); //$NON-NLS-1$
+		btnADD.setFont(new Font(Messages.getString("Key_Checker.22"), Font.BOLD, 7)); 
 		frame.getContentPane().add(btnADD);
 		
 		//Clear Information Button
-		JButton btnCLEAR = new JButton(Messages.getString("Key_Checker.23")); //$NON-NLS-1$
+		JButton btnCLEAR = new JButton(Messages.getString("Key_Checker.23")); 
 		btnCLEAR.setBackground(SystemColor.inactiveCaption);
 		btnCLEAR.setBorder(new BevelBorder(BevelBorder.RAISED, Color.BLUE, null, null, Color.BLUE));
 		btnCLEAR.setBounds(725, 164, 70, 22);
 		btnCLEAR.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				txtNAME_SLOT.setText(Messages.getString("Key_Checker.24")); //$NON-NLS-1$
-				txtKEY_ID_SLOT.setText(Messages.getString("Key_Checker.25")); //$NON-NLS-1$
-				txtDATE_BARROWED_SLOT.setText(Messages.getString("Key_Checker.26")); //$NON-NLS-1$
+				txtNAME_SLOT.setText(Messages.getString("Key_Checker.24")); 
+				txtKEY_ID_SLOT.setText(Messages.getString("Key_Checker.25")); 
+				txtDATE_BARROWED_SLOT.setText(Messages.getString("Key_Checker.26")); 
 				txtDATE_RETURNED_SLOT.setText(null);	
 			}
 		});
 		btnCLEAR.setForeground(Color.BLACK);
-		btnCLEAR.setFont(new Font(Messages.getString("Key_Checker.27"), Font.BOLD, 7)); //$NON-NLS-1$
+		btnCLEAR.setFont(new Font(Messages.getString("Key_Checker.27"), Font.BOLD, 7)); 
 		frame.getContentPane().add(btnCLEAR);
 		
 		//Button delete information
-		JButton btnDELETE = new JButton(Messages.getString("Key_Checker.28")); //$NON-NLS-1$
+		JButton btnDELETE = new JButton(Messages.getString("Key_Checker.28")); 
 		btnDELETE.setBackground(SystemColor.inactiveCaption);
 		btnDELETE.setBorder(new BevelBorder(BevelBorder.RAISED, Color.BLUE, null, null, Color.BLUE));
 		btnDELETE.setBounds(725, 201, 68, 22);
@@ -203,20 +203,20 @@ public class Key_Checker {
 				}
 				else {
 					if (table.getRowCount() == 0 ) {
-						JOptionPane.showMessageDialog(null, Messages.getString("Key_Checker.29")); //$NON-NLS-1$
+						JOptionPane.showMessageDialog(null, Messages.getString("Key_Checker.29")); 
 					}
 					else {
-						JOptionPane.showMessageDialog(null, Messages.getString("Key_Checker.30")); //$NON-NLS-1$
+						JOptionPane.showMessageDialog(null, Messages.getString("Key_Checker.30")); 
 					}
 				}
 			}
 		});
-		btnDELETE.setFont(new Font(Messages.getString("Key_Checker.31"), Font.BOLD, 7)); //$NON-NLS-1$
+		btnDELETE.setFont(new Font(Messages.getString("Key_Checker.31"), Font.BOLD, 7)); 
 		btnDELETE.setForeground(Color.BLACK);
 		frame.getContentPane().add(btnDELETE);
 		
 		//Update information Button
-		JButton btnUPDATE = new JButton(Messages.getString("Key_Checker.32")); //$NON-NLS-1$
+		JButton btnUPDATE = new JButton(Messages.getString("Key_Checker.32")); 
 		btnUPDATE.setBackground(SystemColor.inactiveCaption);
 		btnUPDATE.setBorder(new BevelBorder(BevelBorder.RAISED, Color.BLUE, null, null, Color.BLUE));
 		btnUPDATE.setBounds(647, 201, 68, 22);
@@ -234,26 +234,26 @@ public class Key_Checker {
 					tblModel.setValueAt(Date_Barrowed, table.getSelectedRow(), 2);
 					tblModel.setValueAt(Date_Returned, table.getSelectedRow(), 3);
 					
-					JOptionPane.showMessageDialog(null, Messages.getString("Key_Checker.33"));	 //$NON-NLS-1$
+					JOptionPane.showMessageDialog(null, Messages.getString("Key_Checker.33"));	
 				}
 				else {
 					if (table.getRowCount() == 0) {
-					JOptionPane.showMessageDialog(null, Messages.getString("Key_Checker.34"));	 //$NON-NLS-1$
+					JOptionPane.showMessageDialog(null, Messages.getString("Key_Checker.34"));	
 					}
 					else {
-						JOptionPane.showMessageDialog(null, Messages.getString("Key_Checker.35"));		 //$NON-NLS-1$
+						JOptionPane.showMessageDialog(null, Messages.getString("Key_Checker.35"));		 
 					}
 				}
 			}
 		});
 		
 		btnUPDATE.setForeground(Color.BLACK);
-		btnUPDATE.setFont(new Font(Messages.getString("Key_Checker.36"), Font.BOLD, 7)); //$NON-NLS-1$
+		btnUPDATE.setFont(new Font(Messages.getString("Key_Checker.36"), Font.BOLD, 7)); 
 		frame.getContentPane().add(btnUPDATE);
 		
 		JScrollPane scrollPane_1 = new JScrollPane();
 		scrollPane_1.setBackground(SystemColor.inactiveCaption);
-		scrollPane_1.setFont(new Font(Messages.getString("Key_Checker.37"), Font.BOLD, 12)); //$NON-NLS-1$
+		scrollPane_1.setFont(new Font(Messages.getString("Key_Checker.37"), Font.BOLD, 12)); 
 		scrollPane_1.setBorder(new EtchedBorder(EtchedBorder.RAISED, Color.BLUE, null));
 		scrollPane_1.setBounds(80, 277, 642, 175);
 		frame.getContentPane().add(scrollPane_1);
@@ -275,9 +275,9 @@ public class Key_Checker {
 			}
 		});
 		table.setGridColor(Color.BLUE);
-		table.setFont(new Font(Messages.getString("Key_Checker.38"), Font.PLAIN, 12)); //$NON-NLS-1$
+		table.setFont(new Font(Messages.getString("Key_Checker.38"), Font.PLAIN, 12)); 
 		table.setAlignmentY(Component.TOP_ALIGNMENT);
-		table.setModel(new DefaultTableModel( new Object[][] {}, new String[] { Messages.getString("Key_Checker.39"), Messages.getString("Key_Checker.40"), Messages.getString("Key_Checker.41"), Messages.getString("Key_Checker.42") } //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		table.setModel(new DefaultTableModel( new Object[][] {}, new String[] { Messages.getString("Key_Checker.39"), Messages.getString("Key_Checker.40"), Messages.getString("Key_Checker.41"), Messages.getString("Key_Checker.42") } 
 		));
 		table.getColumnModel().getColumn(0).setMinWidth(20);
 		scrollPane_1.setViewportView(table);
@@ -289,13 +289,13 @@ public class Key_Checker {
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 		
-		JLabel lblKEY_LOG_SYSTEM = new JLabel(Messages.getString("Key_Checker.43")); //$NON-NLS-1$
+		JLabel lblKEY_LOG_SYSTEM = new JLabel(Messages.getString("Key_Checker.43")); 
 		lblKEY_LOG_SYSTEM.setBounds(10, 0, 190, 40);
 		panel.add(lblKEY_LOG_SYSTEM);
 		lblKEY_LOG_SYSTEM.setBackground(SystemColor.activeCaption);
 		lblKEY_LOG_SYSTEM.setBorder(null);
-		lblKEY_LOG_SYSTEM.setToolTipText(Messages.getString("Key_Checker.44")); //$NON-NLS-1$
-		lblKEY_LOG_SYSTEM.setFont(new Font(Messages.getString("Key_Checker.45"), Font.BOLD, 20)); //$NON-NLS-1$
+		lblKEY_LOG_SYSTEM.setToolTipText(Messages.getString("Key_Checker.44")); 
+		lblKEY_LOG_SYSTEM.setFont(new Font(Messages.getString("Key_Checker.45"), Font.BOLD, 20)); 
 		
 		JSeparator separator_1 = new JSeparator();
 		separator_1.setBackground(Color.BLUE);
